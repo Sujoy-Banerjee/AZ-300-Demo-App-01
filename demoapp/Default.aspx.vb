5f42a7a823a0120434da8627b45960f3eb95c4d8
Sujoy <38027171+Sujoy-Banerjee@users.noreply.github.com>
2020-07-03 11:33:26 +0530

    Update Default.aspx.vb

--- demoapp/Default.aspx.vb
+++ demoapp/Default.aspx.vb
@@ -2,7 +2,8 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        System.Diagnostics.Trace.TraceInformation("Test message...AZ-300-Demo-App-01")
+        System.Diagnostics.Trace.WriteLine("Test message...AZ-300-Demo-App-01...");
     End Sub
 
 End Class
