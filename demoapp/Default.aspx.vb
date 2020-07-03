ab9beb9e70c27f1c61fccdce9b11e644807cf99c
Sujoy <38027171+Sujoy-Banerjee@users.noreply.github.com>
2020-07-03 12:39:09 +0530

    Update Default.aspx.vb

--- demoapp/Default.aspx.vb
+++ demoapp/Default.aspx.vb
@@ -4,6 +4,13 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         System.Diagnostics.Trace.TraceInformation("Test message...AZ-300-Demo-App-01")
         System.Diagnostics.Trace.WriteLine("Test message...AZ-300-Demo-App-01...");
+        
+        For value As Integer = 0 To 500
+            ' Step 3: print the current index of the loop.
+            Console.WriteLine("CURRENT FOR-INDEX: {0}", value)
+        Next
+        
+        
     End Sub
 
 End Class
